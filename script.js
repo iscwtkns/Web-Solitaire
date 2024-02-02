984ba7ff190d52286f267e2d160bd7689728dffd
Isaac Watkins <u7320993@anu.edu.au>
2024-02-02 11:34:18 +1100

    Made cards movable and stackable, still only moves one at a time

--- script.js
+++ script.js
@@ -8,6 +8,50 @@ class card {
         this.value = value;
         this.revealed = false;
     }
+
+    returnIntValue() {
+        if (this.value === "A") {
+            return 1;
+        }
+        if (this.value === "J") {
+            return 11;
+        }
+        if (this.value === "Q") {
+            return 12;
+        }
+        if (this.value === "K") {
+            return 13;
+        }
+        else {
+            return parseInt(this.value);
+        }
+    }
+    canPlaceOn(card) {
+        console.log(card.suit);
+        if (this.suit === "club" || this.suit === "spade") {
+            console.log("checking black card");
+            if (card.suit === "heart" || card.suit === "diamond") {
+                console.log("destination red card");
+                console.log(this.returnIntValue());
+                console.log(card.returnIntValue()-1);
+                if (this.returnIntValue() === card.returnIntValue() - 1) {
+                    return true;
+                }
+            }
+        }
+        if (this.suit === "heart" || this.suit === "diamond") {
+            console.log("checking red card");
+            if (card.suit === "club" || card.suit === "spade") {
+                console.log("destination black card");
+                console.log(this.returnIntValue());
+                console.log(card.returnIntValue()-1);
+                if (this.returnIntValue() === card.returnIntValue() - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
 
 //Initialise Game
@@ -26,19 +70,57 @@ setColumns();
 
 function refreshEventHandlers() {
     columns.forEach(function (column, i) {
+        if (column.childNodes.length === 0) {
+            column.addEventListener("click", function moveKing(e) {
+                if (selectedCard !== null) {
+                    let newCard = toCard(selectedCard);
+                    if (newCard.value === "K") {
+                        removeCard(selectedCard);
+                        addCard(i, newCard);
+                        //resetColors();
+                        setColumns();
+                        if (selectedStockPile()) {
+                            drawnPile.pop();
+                            updateStock();
+                        }
+                    }
+                }
+                column.removeEventListener("click", moveKing);
+            })
+        }
         for (let j = 0; j < currentColumns[i].length; j++) {
             column.childNodes.forEach(function(card, j) {
                 if (currentColumns[i][j].revealed) {
-                    card.addEventListener("click", (e) => {
-                        resetColors();
-                        selectedCard = card;
-                        card.style.backgroundColor = "aqua";
+                    card.addEventListener("click", function handleClick(e) {
+                        if (selectedCard === null) {
+                            console.log("selecting first card");
+                            resetColors();
+                            selectedCard = card;
+                            card.style.backgroundColor = "aqua";
+                        } else if (toCard(selectedCard).canPlaceOn(toCard(card))) {
+                            let newCard = toCard(selectedCard);
+                            if (selectedStockPile()) {
+                                drawnPile.pop();
+                                updateStock();
+                            }
+                            removeCard(selectedCard);
+                            addCard(i, newCard);
+                            selectedCard = null;
+                            revealCards();
+                            setColumns();
+                            resetColors();
+                        } else {
+                            resetColors();
+                            selectedCard = card;
+                            card.style.backgroundColor = "aqua";
+                        }
+                        // Remove the event listener after it's been executed once
                     })
                 }
             })
         }
     })
-    console.log(stock.firstChild);
+
     if (stock.firstChild !== null) {
         stock.firstChild.addEventListener("click", (e) => {
             resetColors();
@@ -46,10 +128,53 @@ function refreshEventHandlers() {
             selectedCard.style.backgroundColor = "aqua";
         })
     }
+    foundationSpots.forEach(function(spot, index) {
+        spot.addEventListener("click", (e) => {
+            if (selectedCard !== null) {
+                if (selectedStockPile()) {
+                    drawnPile.pop();
+                    updateStock();
+                }
+                attemptFoundation();
+            }
+            if (spot.firstChild !== null && selectedCard === null) {
+                selectedCard = spot.firstChild;
+                selectedCard.style.backgroundColor = "aqua";
+            }
+        })
+
+
+    })
 }
 
+function selectedStockPile() {
+    return selectedCard === stock.firstChild;
+}
+function addCard(column, card) {
+    card.revealed = true;
+    currentColumns[column].push(card);
+    console.log(card);
+    console.log(currentColumns);
+}
+function toCard(vcard) {
+    let value = "";
+    let suit = "";
+    vcard.firstChild.childNodes.forEach(function(node, index) {
+        if (index === 0) {
+            value = node.textContent;
+        }
+        else {
+            suit = getSuitFromImagePath(node.src);
+        }
+    })
+    let card1 = new card(suit, value);
+    card1.revealed = true;
+    return card1;
+}
 function attemptFoundation() {
-    console.log("attempting to move to foundation");
+    if (selectedCard === null) {
+        return;
+    }
     let value = "";
     let suit = "";
     selectedCard.firstChild.childNodes.forEach(function(info, index) {
@@ -61,7 +186,6 @@ function attemptFoundation() {
         }
     })
     if (selectedCard === null) {
-        console.log("no card selected")
         return;
     }
     let value1 = 0;
@@ -80,15 +204,11 @@ function attemptFoundation() {
     else {
         value1 = parseInt(value);
     }
-    console.log(value);
-    console.log(value1);
-    console.log(suit);
+
 
     if (suit === "spade") {
-        console.log("chose spade");
         if (foundation[0] === value1 - 1) {
             removeCard(selectedCard);
-            console.log("successful move");
             foundation[0] = value1;
             drawFoundation(new card(suit, value), selectedCard);
         }
@@ -98,10 +218,8 @@ function attemptFoundation() {
         }
     }
     if (suit === "heart") {
-        console.log("chose heart");
 
         if (foundation[1] === value1 - 1) {
-            console.log("successful move");
             removeCard(selectedCard);
 
             foundation[1] = value1;
@@ -113,10 +231,8 @@ function attemptFoundation() {
         }
     }
     if (suit === "diamond") {
-        console.log("chose diamond");
 
         if (foundation[2] === value1 - 1) {
-            console.log("successful move");
             removeCard(selectedCard);
 
             foundation[2] = value1;
@@ -128,10 +244,8 @@ function attemptFoundation() {
         }
     }
     if (suit === "club") {
-        console.log("chose club");
 
         if (foundation[3] === value1 - 1) {
-            console.log("successful move");
             removeCard(selectedCard);
 
             foundation[3] = value1;
@@ -148,8 +262,8 @@ function attemptFoundation() {
     setColumns();
     refreshEventHandlers();
 }
-
 function removeCard(card) {
+    console.log(currentColumns);
     let value = card.firstChild.firstChild.textContent;
     let suit = "";
     card.firstChild.childNodes.forEach(function(child, index) {
@@ -160,16 +274,18 @@ function removeCard(card) {
     for (let i = 0; i < 7; i++) {
         for (let j = 0; j < currentColumns[i].length; j++) {
             if (suit === currentColumns[i][j].suit && value === currentColumns[i][j].value) {
-                currentColumns[i].pop();
+                console.log("removing card");
+                currentColumns[i].splice(j, 1);
+                break;
             }
         }
     }
+    console.log(currentColumns);
 
 }
 function getSuitFromImagePath(imagePath) {
     // Extract the filename from the image path
     const filename = imagePath.split("/").pop();
-    console.log(filename);
     // Determine the suit based on the filename
     if (filename.includes("spade")) {
         return "spade";
@@ -183,7 +299,6 @@ function getSuitFromImagePath(imagePath) {
         return "unknown";
     }
 }
-
 function drawFoundation(card, vcard) {
     vcard.className = "foundationCard";
     if (card.suit === "spade") {
@@ -204,12 +319,14 @@ function drawFoundation(card, vcard) {
     }
 
 }
-
 function revealCards() {
     for (let i = 0; i < currentColumns.length; i++) {
         for (let j = 0; j < currentColumns[i].length; j++) {
+            console.log(currentColumns[i][j], i, j);
             if (j === currentColumns[i].length - 1) {
+
                 currentColumns[i][j].revealed = true;
+
             }
         }
     }
@@ -234,11 +351,55 @@ function resetColors() {
         stock.firstChild.style.backgroundColor = "white";
     }
 }
+function updateStock() {
+    removeAllChildNodes(stock);
+    if (drawnPile.length === 0) {
+        return;
+    }
+    console.log(drawnPile);
+    let card = drawnPile[drawnPile.length-1];
+    console.log(card);
+    let cardDetails = document.createElement("div");
+    cardDetails.className = "cardDetails";
+    let cardText = document.createElement("p");
+    let smallIcon = document.createElement("img");
+    smallIcon.className = "smallIcon";
+    let largeIcon = document.createElement("img");
+    largeIcon.className = "largeIcon";
+    const suit = card.suit;
+    cardText.textContent = card.value;
+    if (suit === "spade") {
+        cardText.style.color = "black";
+        smallIcon.src = "icons/spade.png";
+        largeIcon.src = "icons/spade.png";
+    }
+    if (suit === "heart") {
+        cardText.style.color = "#c71515";
+        smallIcon.src = "icons/heart.png";
+        largeIcon.src = "icons/heart.png";
+    }
+    if (suit === "diamond") {
+        cardText.style.color = "#c71515";
+        smallIcon.src = "icons/diamond.png";
+        largeIcon.src = "icons/diamond.png";
+    }
+    if (suit === "club") {
+        cardText.style.color = "black";
+        smallIcon.src = "icons/club.png";
+        largeIcon.src = "icons/club.png";
+    }
+    cardDetails.appendChild(cardText);
+    cardDetails.appendChild(smallIcon);
+    let vcard = document.createElement("div");
+    vcard.className = "stockCard";
+    vcard.appendChild(cardDetails);
+    vcard.appendChild(largeIcon);
+    stock.appendChild(vcard);
+}
 function drawFromStock() {
     removeAllChildNodes(stock);
     if (stockPile.length === 0) {
         stockPile = drawnPile;
-        console.log(stockPile);
         drawnPile = [];
         hiddenCardSymbol.style.opacity = 1;
     }
@@ -334,6 +495,7 @@ function setColumns() {
     columns.forEach(function(column, index) {
         removeAllChildNodes(column);
         for (let i = 0; i < currentColumns[index].length; i++) {
+            console.log("column " + index + " has length " + currentColumns[index].length);
             if (currentColumns[index][i].revealed === true) {
                 let cardDetails = document.createElement("div");
                 cardDetails.className = "cardDetails";
@@ -380,6 +542,7 @@ function setColumns() {
             }
         }
     })
+    refreshEventHandlers();
 }
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
